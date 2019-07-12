ff9a4441279ada9d3d075b676bbd428e1ca9689f
Sirikonss <sirikon.exo@gmail.com>
2019-07-12 15:36:26 +0700

    add

--- script.js
+++ script.js
@@ -69,3 +69,4 @@ for (var i=0;i<6;i++) {
 res =  temp[0] + ":" + temp[1] + ":" + temp[2] + " " + temp[3] + ":" + temp[4] + ":" + temp[5] ; 
 document.getElementById("timeinterval").innerHTML = `<h3>${res}</h3>`                     
 }
+
